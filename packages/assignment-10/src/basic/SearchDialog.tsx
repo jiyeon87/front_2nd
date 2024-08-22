a9bea633a0a5d1209dd4e478f9ee6b93834ff607
jiyeon87 <jenny87879@gmail.com>
2024-08-23 06:01:42 +0900

    refactor: Promise.all문 수정 및 API 호출 개선

--- packages/assignment-10/src/basic/SearchDialog.tsx
+++ packages/assignment-10/src/basic/SearchDialog.tsx
@@ -82,18 +82,43 @@ const TIME_SLOTS = [
 
 const PAGE_SIZE = 100;
 
-const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json');
-const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-liberal-arts.json');
+function attachCache<T>(fetch: () => Promise<T>): () => Promise<T> {
+  let cache: T | undefined;
+  let fetchPromise: Promise<T> | null = null;
 
-// TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-const fetchAllLectures = async () => await Promise.all([
-  (console.log('API Call 1', performance.now()), await fetchMajors()),
-  (console.log('API Call 2', performance.now()), await fetchLiberalArts()),
-  (console.log('API Call 3', performance.now()), await fetchMajors()),
-  (console.log('API Call 4', performance.now()), await fetchLiberalArts()),
-  (console.log('API Call 5', performance.now()), await fetchMajors()),
-  (console.log('API Call 6', performance.now()), await fetchLiberalArts()),
-]);
+  return async function () {
+    if (cache) {
+      return cache;
+    }
+
+    if (!fetchPromise) {
+      fetchPromise = fetch().then((result) => {
+        cache = result;
+        fetchPromise = null;
+        return result;
+      });
+    }
+
+    return fetchPromise;
+  };
+}
+
+const fetchMajors = attachCache(() =>
+  axios.get<Lecture[]>('/schedules-majors.json')
+);
+const fetchLiberalArts = attachCache(() =>
+  axios.get<Lecture[]>('/schedules-liberal-arts.json')
+);
+
+const fetchAllLectures = async () =>
+  await Promise.all([
+    (console.log('API Call 1', performance.now()), fetchMajors()),
+    (console.log('API Call 2', performance.now()), fetchLiberalArts()),
+    (console.log('API Call 3', performance.now()), fetchMajors()),
+    (console.log('API Call 4', performance.now()), fetchLiberalArts()),
+    (console.log('API Call 5', performance.now()), fetchMajors()),
+    (console.log('API Call 6', performance.now()), fetchLiberalArts()),
+  ]);
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
